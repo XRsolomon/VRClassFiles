1c31db9992670dc29c6f2f9fd6799477fa1a7f41
XRLibraries <53194833+XRLibraries@users.noreply.github.com>
2019-09-10 04:25:49 -0700

    added probuilder and xrtracker

---
+++ VR Academy Fall 2019/Assets/Scripts/ControllerInput.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.XR;
+
+[RequireComponent(typeof(XRTracker))]
+
+public class ControllerInput : MonoBehaviour
+{
+
+
+
+    public UnityEvent triggerPressed;
+    public UnityEvent gripPressed;
+
+    XRTracker tracker;
+    float triggerInputValue;
+    float gripInputValue;
+
+    bool triggerState;
+    bool gripState;
+
+    private void Start()
+    {
+        tracker = GetComponent<XRTracker>();
+    }
+
+    private void Update()
+    {
+        tracker.trackedDevice.TryGetFeatureValue(CommonUsages.trigger, out triggerInputValue);
+
+        if (triggerInputValue > .5f && triggerState == false)
+        {
+            triggerPressed.Invoke();
+            triggerState = true;
+        }
+        else if(triggerInputValue < .5f)
+        {
+            triggerState = false;
+        }
+
+        tracker.trackedDevice.TryGetFeatureValue(CommonUsages.grip, out gripInputValue);
+
+        if(gripInputValue > .5f && gripState == false)
+        {
+            gripState = true;
+            gripPressed.Invoke();
+        }
+        else if (gripInputValue < .5f)
+        {
+            gripState = false;
+        }
+
+    }
+}
---
+++ VR Academy Fall 2019/Assets/Scripts/XRTracker.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class XRTracker : MonoBehaviour
+{
+    //Keep this around to avoid creating heap garbage
+    static List<InputDevice> devices = new List<InputDevice>();
+
+    public InputDeviceRole role;
+    [HideInInspector]
+    public InputDevice trackedDevice;
+    void OnEnable()
+    {
+        InputDevices.deviceConnected += OnDeviceConnected;
+        InputDevices.deviceDisconnected += OnDeviceDisconnected;
+        Application.onBeforeRender += OnBeforeRender;
+        InputDevices.GetDevicesWithRole(role, devices);
+        if (devices.Count > 0)
+            OnDeviceConnected(devices[0]);
+    }
+    void OnDisable()
+    {
+        InputDevices.deviceConnected -= OnDeviceConnected;
+        InputDevices.deviceDisconnected -= OnDeviceDisconnected;
+        Application.onBeforeRender -= OnBeforeRender;
+    }
+    void Update()
+    {
+        if (trackedDevice.isValid)
+            TrackToDevice(trackedDevice);
+    }
+    void OnDeviceConnected(InputDevice device)
+    {
+        if (!trackedDevice.isValid && device.role == role)
+            trackedDevice = device;
+    }
+    void OnDeviceDisconnected(InputDevice device)
+    {
+        if (device == trackedDevice)
+            trackedDevice = new InputDevice();
+    }
+    void OnBeforeRender()
+    {
+        if (trackedDevice.isValid)
+            TrackToDevice(trackedDevice);
+    }
+    void TrackToDevice(InputDevice trackedDevice)
+    {
+        Vector3 position;
+        if (trackedDevice.TryGetFeatureValue(CommonUsages.devicePosition, out position))
+            this.transform.position = position;
+        Quaternion rotation;
+        if (trackedDevice.TryGetFeatureValue(CommonUsages.deviceRotation, out rotation))
+            this.transform.rotation = rotation;
+    }
+}
